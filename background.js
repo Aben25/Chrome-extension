263381fbaa3031b242a10df179b3eb3a46e4333e
Abenezer Nuro <anuro@artba.org>
2023-05-22 05:47:50 -0400

    mrny

--- background.js
+++ background.js
@@ -1,54 +1,39 @@
-
-
-// let summary = '';  // Variable to store the summary
-
-// chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-//   if (request.action == "summarize") {
-//     console.log('Received summarize request:', request.text);
-//     fetch('http://localhost:3000/summarize', {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: JSON.stringify({text: request.text})
-//     })
-//     .then(response => response.json())
-//     .then(data => {
-//       console.log('Received summary from server:', data.summary);
-//       summary = data.summary;  // Store the summary
-//       sendResponse({summary: data.summary});
-//     })
-//     .catch(error => console.error('Error:', error));
-//   } else if (request.action == "getSummary") {
-//     console.log('Received getSummary request');
-//     sendResponse({summary: summary});  // Send the stored summary
-//   }
-//   return true; // Keeps the message channel open until sendResponse is called
-// });
-chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
-    if (request.action == "summarize") {
-      fetch('http://localhost:3000/summarize', {
-        method: 'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify({ text: request.text })
-      })
-        .then(response => response.json())
-        .then(data => {
-          const summary = data.result;
-          chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-            chrome.tabs.sendMessage(tabs[0].id, { action: "displaySummary", summary: summary });
-          });
-          sendResponse({ summary: summary });
-        })
-        .catch(error => console.error('Error:', error));
-    } else if (request.action == "getSummary") {
-      // Retrieve the summary from storage (e.g., variable or database)
-      const summary = 'This is a sample summary'; // Replace with the actual summary
-  
-      sendResponse({ summary: summary });
-    }
-    return true; // Keeps the message channel open until sendResponse is called
+chrome.runtime.onInstalled.addListener(function() {
+  chrome.contextMenus.create({
+    id: "summarizeContextMenu",
+    title: "Summarize Selected Text",
+    contexts: ["selection"],
   });
-  
+});
+
+chrome.contextMenus.onClicked.addListener(function(info, tab) {
+  if (info.menuItemId === "summarizeContextMenu") {
+    chrome.tabs.sendMessage(tab.id, { action: "summarize" });
+    
+  }
+});
+
+chrome.runtime.onMessage.addListener(function(request, sender, sendResponse) {
+  if (request.action === "summarize") {
+    fetch('http://localhost:3000/summarize', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({ text: request.text }),
+    })
+      .then(response => response.json())
+      .then(data => {
+        const summary = data.result;
+        chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+          chrome.tabs.sendMessage(tabs[0].id, { action: "displaySummary", summary: summary });
+        });
+        sendResponse({ summary: summary });
+      })
+      .catch(error => console.error('Error:', error));
+  } else if (request.action === "getSummary") {
+    const summary = 'This is a sample summary'; // Replace with the actual summary
+    sendResponse({ summary: summary });
+  }
+  return true;
+});
--- popup.js
+++ popup.js
@@ -1,35 +1,48 @@
 document.addEventListener('DOMContentLoaded', function() {
-    document.getElementById('summarize').addEventListener('click', function() {
-      chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
-        let tab = tabs[0];
-        chrome.scripting.executeScript({
+  const loadingElement = document.getElementById('loading');
+  const summaryElement = document.getElementById('summary');
+
+  document.getElementById('summarize').addEventListener('click', function() {
+    loadingElement.classList.remove('hidden');
+    summaryElement.classList.add('hidden');
+
+    chrome.tabs.query({ active: true, currentWindow: true }, function(tabs) {
+      let tab = tabs[0];
+      chrome.scripting.executeScript(
+        {
           target: { tabId: tab.id },
-          func: getSelectedText
-        }, function(result) {
+          function: getSelectedText,
+        },
+        function(result) {
           const selectedText = result[0].result;
           chrome.runtime.sendMessage({ action: "summarize", text: selectedText }, function(response) {
             const summary = response.summary;
-            document.getElementById('summary').textContent = summary;
-            readText(summary); // Read the summarized text aloud
+            loadingElement.classList.add('hidden');
+            summaryElement.classList.remove('hidden');
+            summaryElement.textContent = summary;
+            readText(summary);
           });
-        });
-      });
-    });
-  
-    function getSelectedText() {
-      return window.getSelection().toString();
-    }
-  
-    function readText(text) {
-      const speechSynthesis = window.speechSynthesis;
-      const utterance = new SpeechSynthesisUtterance(text);
-      speechSynthesis.speak(utterance);
-    }
-  
-    // Request the summary from the background script when the popup is opened
-    chrome.runtime.sendMessage({ action: "getSummary" }, function(response) {
-      const summary = response.summary;
-      document.getElementById('summary').textContent = summary;
+        }
+      );
     });
   });
-  
+
+  function getSelectedText() {
+    return window.getSelection().toString();
+  }
+
+  function readText(text) {
+    const speechSynthesis = window.speechSynthesis;
+    const utterance = new SpeechSynthesisUtterance(text);
+    speechSynthesis.speak(utterance);
+  }
+
+  // Request the summary from the background script when the popup is opened
+  chrome.runtime.sendMessage({ action: "getSummary" }, function(response) {
+    const summary = response.summary;
+    loadingElement.classList.add('hidden');
+    summaryElement.classList.remove('hidden');
+    summaryElement.textContent = summary;
+  });
+});
+
